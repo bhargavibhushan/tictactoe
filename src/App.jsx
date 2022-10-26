4b5af10e4bc59c4cb122c63114ad59e579a11d3f
Bhargavi <bhargavibhushan99@gmail.com>
2022-10-26 20:16:03 +0530

    new changes

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <>
+      <h1>Welcome to React Vite Micro App!</h1>
+      <h2>Iam Loving It</h2>
+      <h3> Lets Learn React Together</h3>
+      <p>Hard to get more minimal than this React Rock app...</p>
+    </>
+  );
+};
+export default App;
